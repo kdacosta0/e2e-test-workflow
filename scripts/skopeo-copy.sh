b31162369775ecf8b066cc917d3893ae4a5b5689
Kristián Da Costa Menezes <113685889+kdacosta0@users.noreply.github.com>
2024-07-03 12:55:23 +0200

    debugging

--- scripts/skopeo-copy.sh
+++ scripts/skopeo-copy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -e
 
-UBUNTU_IMAGE="ttl.sh/$(uuidgen):10m"
-
-skopeo copy docker://docker.io/library/alpine:latest docker://$UBUNTU_IMAGE
+skopeo copy docker://docker.io/library/alpine:latest docker://${UBUNTU_IMAGE}
+skopeo copy docker://docker.io/library/alpine:latest docker://${WINWOWS_IMAGE}
+skopeo copy docker://docker.io/library/alpine:latest docker://${MACOS_IMAGE}
