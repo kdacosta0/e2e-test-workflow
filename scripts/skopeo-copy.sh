75a53dc4de15c090faba7b2de8c689958b62ee8b
Kristián Da Costa Menezes <113685889+kdacosta0@users.noreply.github.com>
2024-07-03 13:09:25 +0200

    debugging

--- scripts/skopeo-copy.sh
+++ scripts/skopeo-copy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Generate unique image names
-UBUNTU_IMAGE="ttl.sh/$(uuidgen):10m"
-WINDOWS_IMAGE="ttl.sh/$(uuidgen):10m"
-MACOS_IMAGE="ttl.sh/$(uuidgen):10m"
-
 # Copy images to registry using skopeo
 skopeo copy docker://docker.io/library/alpine:latest docker://${UBUNTU_IMAGE}
-skopeo copy docker://docker.io/library/alpine:latest docker://${WINWOWS_IMAGE}
+skopeo copy docker://docker.io/library/alpine:latest docker://${WINDOWS_IMAGE}
 skopeo copy docker://docker.io/library/alpine:latest docker://${MACOS_IMAGE}
 
 # Export environment variables to be available for subsequent steps
